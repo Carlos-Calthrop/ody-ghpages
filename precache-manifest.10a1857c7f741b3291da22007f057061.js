c20447ab4f8f68c089230b9f403c9b66081db2ed
carlos <Carlos.Calthrop@odysseuslabs.io>
2020-08-03 09:20:45 -0400

    Updates

---
+++ precache-manifest.10a1857c7f741b3291da22007f057061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eda382c94b55806c4c007dc9965a949",
+    "url": "/ody-ghpages/index.html"
+  },
+  {
+    "revision": "1dc6d21719999ea80a21",
+    "url": "/ody-ghpages/static/css/2.360b7c10.chunk.css"
+  },
+  {
+    "revision": "df8fdb7c207b3b058e2f",
+    "url": "/ody-ghpages/static/css/main.7ca4982c.chunk.css"
+  },
+  {
+    "revision": "1dc6d21719999ea80a21",
+    "url": "/ody-ghpages/static/js/2.37c07a7f.chunk.js"
+  },
+  {
+    "revision": "78854e6af10aa71eb7c9b59341d485ab",
+    "url": "/ody-ghpages/static/js/2.37c07a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8fdb7c207b3b058e2f",
+    "url": "/ody-ghpages/static/js/main.c36c751c.chunk.js"
+  },
+  {
+    "revision": "6594001eefde22710930",
+    "url": "/ody-ghpages/static/js/runtime-main.5a6c9758.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.d3d3570e96cfe42ab2324729cdbbb769.js"
+  "/ody-ghpages/precache-manifest.10a1857c7f741b3291da22007f057061.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ody-ghpages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
